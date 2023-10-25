7c0a42cbbfe5bce847258dc6f31430ac870c7ee0
HoHong123 <epzmfhs@gmail.com>
2023-10-26 03:04:43 +0900

    ✏️ Comment : Add new comments in FnStrcmp.c script

--- KiHong/week5/FnStrcmp.c
+++ KiHong/week5/FnStrcmp.c
@@ -3,7 +3,7 @@
 int fn_strcmp(const char *string1, const char *string2);
 
 int main(void) {
-    char *string1 = "comparet";
+    char *string1 = "compare";
     char *string2 = "compares";
 
     int result = fn_strcmp(string1, string2);
@@ -20,11 +20,20 @@ int main(void) {
     return 0;
 }
 
+// <summary> compare two char array
+// <parameter=*string1> first string
+// <parameter=*string2> second string
 int fn_strcmp(const char *string1, const char *string2) {
-    while (*string1 && *string2 && *string1 == *string2) {
-        string1++;
-        string2++;
+    // Force to convert char* to unsigned char*
+    // This is a important task since input value can be a negative char value.
+    // Negative char can cause serious issues, such as stackoverflow. Which can lead to security issues.
+    unsigned char *s1 = (unsigned char*)string1;
+    unsigned char *s2 = (unsigned char*)string2;
+
+    while (*s1 && *s2 && *s1 == *s2) {
+        s1++;
+        s2++;
     }
     // Calculate in ASKII code
-    return *string1 - *string2;
+    return *s1 - *s2;
 }
