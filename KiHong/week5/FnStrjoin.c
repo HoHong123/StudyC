bc83413ff3eb8615096b7fbe998e90040fc2bdad
HoHong123 <epzmfhs@gmail.com>
2023-11-07 18:54:10 +0900

    ✨ feat : [23.11.07][KiHong] Add FnSplit, FnStrjoin, FnStrdup

--- KiHong/week5/FnStrjoin.c
+++ KiHong/week5/FnStrjoin.c
@@ -3,3 +3,28 @@
 char *fn_strjoin(const char *s1, const  char *s2)
 */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char *fn_strjoin(const char *s1, const  char *s2);
+
+int main(void) {
+    char *test1 = "Copy this";
+    char *test2 = ", And this";
+    char *test3;
+
+   if ((test3 = fn_strjoin(test1, test2)) != NULL)
+      printf("The new string is: %s\n", test3);
+
+    return 0;
+}
+
+char *fn_strjoin(const char *s1, const  char *s2) {
+    char* result = (char*)malloc(strlen(s1) + strlen(s2) + 1);
+
+    strcat(result, s1);
+    strcat(result, s2);
+
+    return result;
+}
